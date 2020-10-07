c6bd504276676d5e5b4399ab21e3aff39c2f9d5e
chz <1074040117@qq.com>
2020-10-07 15:46:04 +0800

    add config file

--- src/utils/loginRequest.js
+++ src/utils/loginRequest.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const loginService = axios.create({
     // api的url地址
-    // baseURL: 'https://admin-api.claps.dev/',
-    baseURL: 'http://localhost:7003/',
+    //baseURL: 'https://admin-api.claps.dev/',
+    baseURL: process.env.VUE_APP_BASE_URL,
     // 持续时间2分钟
     timeout: 1000 * 120,
     // token验证请求的有效性
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
 import storageService from "../service/storageService";
+//import Vue from 'vue';
 
 const service = axios.create({
     // api的url地址
-    // baseURL: 'https://admin-api.claps.dev/',
-    baseURL: 'http://localhost:7003/',
+    //baseURL: 'https://admin-api.claps.dev/',
+    baseURL: process.env.VUE_APP_BASE_URL,
+    //baseURL: Vue.prototype.baseConfig,
     // 持续时间2分钟
     timeout: 1000 * 120,
     // token验证请求的有效性
